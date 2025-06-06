eddd25aea72df539c25c0217017f4fe59a971e99
stastnyjakub <jakub.stastny@koala42.com>
2025-06-06 19:10:10 +0200

    🚀 feat: add mobile app

---
+++ apps/mobile/eslint.config.js
@@ -0,0 +1,16 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    plugins: {
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+    ignores: ['dist/*'],
+  },
+]);
